584cfe4f0954c4f6a7e36ce16967240b81493d0d
osc-sh <48035650+osc-ema@users.noreply.github.com>
2020-06-16 19:02:43 +0200

    Rocket Launch

---
+++ osc-usw/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    GameObject LaunchButton;
+    GameObject EnvironmentBaked;
+    GameObject EnvironmentUnbaked;
+
+    void Start()
+    {
+        LaunchButton = GameObject.Find("LaunchButton");
+        LaunchButton.SetActive(false);
+        EnvironmentBaked = GameObject.Find("EnvironmentBaked");
+        //EnvironmentBaked.SetActive(true);
+        EnvironmentUnbaked = GameObject.Find("EnvironmentUnbaked");
+        EnvironmentUnbaked.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LaunchButton.SetActive(true);
+            EnvironmentBaked.SetActive(false);
+            EnvironmentUnbaked.SetActive(true);
+        
+        }
+    
+    }
+}
---
+++ osc-usw/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Valve.VR.InteractionSystem;
+
+public class EventHandler : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+    public void OnButtonPress()
+    {
+        timeline.Play();
+    }
+}
